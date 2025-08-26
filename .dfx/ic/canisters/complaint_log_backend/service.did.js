c1551a784e1df0c97562a8368e30942446a596e0
akilesh464 <sakileshixe@gmail.com>
2025-08-27 02:11:12 +0530

    updated
    new files added

--- .dfx/ic/canisters/complaint_log_backend/service.did.js
+++
@@ -1,8 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'addComplaint' : IDL.Func([IDL.Text, IDL.Text], [IDL.Bool], []),
-    'getComplaintCount' : IDL.Func([], [IDL.Int], ['query']),
-    'getData' : IDL.Func([], [IDL.Vec(IDL.Vec(IDL.Text))], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
